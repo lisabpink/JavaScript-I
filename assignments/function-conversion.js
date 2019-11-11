60f97953bc8e79d24c5fb000304b241416d2a0ce
LISA_COOK <lisabpink@gmail.com>
2019-11-11 15:48:34 -0500

    Initial push Lisa Cook2

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,50 +5,20 @@
 // };
 // myFunction();
 
-let myFunction =() =>{
-    console.log("Function was invoked!");
-}
-console.log(myFunction);
-
-
-
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
-
-
-let anotherFunction = (param) =>{
-    return param;
-};
-
-
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
-
-let add = (param1, param2) =>{
-    return param1 + param2;
-};
- console.log(add(1,2))
-
-
-
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
-
-let subtract = (param1, param2) => {
-    return param1 - param2;
-};
-console.log(subtract(1,2))
-
-
 
 
 // Stretch
@@ -57,4 +27,4 @@ console.log(subtract(1,2))
 // const triple = exampleArray.map(function (num) {
 //   return num * 3;
 // });
-// console.log(triple);  
+// console.log(triple);
